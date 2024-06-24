b2c0e1533dc97fc247c8c6c595b4eaee5288aa8b
Alimhan <56984209+Ailmhan@users.noreply.github.com>
2024-06-24 16:29:12 +0600

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { auth } from '@/auth';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Next Shadcn',
-  description: 'Basic dashboard with Next.js and Shadcn'
+  title: 'Belford AI',
+  description: 'AI Sales Department'
 };
 
 export default async function RootLayout({
